f049de437c81993f7c85c8dc01a9c8b47fa08847
vijitha <kondapurvr@gmil.com>
2021-02-27 19:22:36 +0530

    hello postman

---
+++ Desktop/Postman/Testing.postman_globals.json.js
@@ -0,0 +1,16 @@
+{
+  "id": "3cb97ee9-9901-313f-625d-ee27250af46d",
+  "name": "Postman Globals",
+  "values": [
+    {
+      "key": "url",
+      "value": "https://jsonplaceholder.typicode.com",
+      "description": "",
+      "type": "text",
+      "enabled": true
+    }
+  ],
+  "_postman_variable_scope": "globals",
+  "_postman_exported_at": "2021-02-25T07:51:34.058Z",
+  "_postman_exported_using": "Postman/5.5.5"
+}
